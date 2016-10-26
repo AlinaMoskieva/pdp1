596e7ab4c4966e7e4c45bccd8c88a9f87fedb245
Alina Moskieva <alinamoskieva@gmail.com>
2016-10-26 15:37:55 +0300

    Visitor can view About Us page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def about_us
+    render template: "pages/about_us"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "users/registrations" }
   root to: "pages#home"
+
+  get "about_us", to: "pages#about_us"
 end
