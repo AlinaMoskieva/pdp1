55629da16f577a314bd801c8f4dcab58e0c23cb9
Alina Moskieva <alinamoskieva@gmail.com>
2016-11-02 14:44:20 +0300

    restrict article resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :articles
+  resources :articles, only: %i(show create new index edit update destroy)
   devise_for :users, controllers: { registrations: "users/registrations" }
   root to: "articles#index"
 end
