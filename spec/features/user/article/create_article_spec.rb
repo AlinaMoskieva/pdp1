5f1d7f00e423ec43683193d7de5f280e75536906
Alina Moskieva <alinamoskieva@gmail.com>
2016-11-02 13:31:33 +0300

    article's tests

--- spec/features/user/article/create_article_spec.rb
+++ spec/features/user/article/create_article_spec.rb
@@ -1,26 +1,38 @@
 require "rails_helper"
 
-feature "As user I able to" do
+feature "User creates" do
   let(:user) { create :user }
   let(:title) { Faker::Lorem.sentence(3) }
   let(:text) { Faker::Lorem.sentence(3) }
 
-  describe "create" do
-    background do
-      login_as user
-      visit articles_path
-    end
+  before do
+    login_as user
+    visit articles_path
+  end
 
-    scenario "article" do
-      expect(page).to have_link("New Article")
-      click_link "New Article"
+  def find_new_article_link
+    expect(page).to have_link("New Article")
+    click_link "New Article"
+  end
 
-      fill_in "Title", with: title
-      fill_in "Text", with: text
-      click_button "Create Article"
+  scenario "article with valid data" do
+    find_new_article_link
 
-      expect(page).to have_content(title)
-      expect(page).to have_content(text)
-    end
+    fill_in "Title", with: title
+    fill_in "Text", with: text
+    click_button "Create Article"
+
+    expect(page).to have_content(title)
+    expect(page).to have_content(text)
+  end
+
+  scenario "article with invalid data" do
+    find_new_article_link
+
+    fill_in "Title", with: ""
+    fill_in "Text", with: ""
+    click_button "Create Article"
+
+    expect(page).to have_content("Article could not be created.")
   end
 end
---
+++ spec/features/user/article/update_article_spec.rb
@@ -0,0 +1,49 @@
+require "rails_helper"
+
+feature "User updates" do
+  let!(:user) { create :user }
+  let!(:article) { create :article, user: user }
+  let!(:another_article) { create :article }
+  let(:title) { Faker::Lorem.sentence(3) }
+  let(:text) { Faker::Lorem.sentence(3) }
+
+  before do
+    login_as user
+    visit article_path(article)
+  end
+
+  def find_edit_article_link
+    within ".form-actions" do
+      expect(page).to have_link("Edit")
+      click_link("Edit")
+    end
+  end
+
+  scenario "comment with valid data" do
+    find_edit_article_link
+
+    fill_in "Title", with: title
+    fill_in "Text", with: text
+    click_button "Update Article"
+
+    expect(page).to have_content(title)
+    expect(page).to have_content(text)
+  end
+
+  scenario "comment with invalid data" do
+    find_edit_article_link
+
+    fill_in "Title", with: ""
+    fill_in "Text", with: ""
+    click_button "Update Article"
+
+    expect(page).to have_content("Article could not be updated.")
+  end
+
+  scenario "not his comment" do
+    visit article_path(another_article)
+    within ".form-actions" do
+      expect(page).not_to have_link("Edit")
+    end
+  end
+end
